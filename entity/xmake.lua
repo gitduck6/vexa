549802040e3bb600247c3690cca94e5e94371b29
Monjaris <200353384+Monjaris@users.noreply.github.com>
2026-08-25 00:45:16 +0400

    untrack: entity/ because it is too immature for production

--- entity/xmake.lua
+++
@@ -1,7 +0,0 @@
-set_languages("c++23") -- bump to c++26 once your toolchain's std flag supports it
-add_rules("mode.debug", "mode.release")
-
-target("ecs_demo")
-    set_kind("binary")
-    add_files("main.cpp")
-    add_includedirs(".")
