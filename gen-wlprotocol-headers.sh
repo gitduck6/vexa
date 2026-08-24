341e0ac18513313b38295c4d15826ed832b56245
Monjaris <200353384+Monjaris@users.noreply.github.com>
2026-08-25 00:02:03 +0400

    remove gen-protocol-headers.sh

--- gen-wlprotocol-headers.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p vendor/SDL3/src/video/wayland/generated
-for xml in vendor/SDL3/wayland-protocols/*.xml; do
-  b=$(basename "$xml" .xml)
-  wayland-scanner client-header "$xml" vendor/SDL3/src/video/wayland/generated/${b}-client-protocol.h
-  wayland-scanner private-code "$xml" vendor/SDL3/src/video/wayland/generated/${b}-protocol.c
-done
